23f9a2b2a4334a55c31045e88a92d93465ce4076
Slavcheva <stoyka.slavcheva@gmail.com>
2023-12-10 16:23:10 +0200

    Add functions to upload and sanitize data

--- src/components/templates/Main/Main.jsx
+++ src/components/templates/Main/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Route, Routes} from "react-router-dom";
-
+import {useState} from "react";
 import "./Main.css"
 import Navigation from "../../organisms/Navigation/Navigation";
 import GeneralStatistic from "../../pages/GeneralStatistic";
@@ -9,13 +9,39 @@ import MostPointsScoredByTeamTable from "../../pages/MostPointsScoredByTeamTable
 import MostPointsScoredInAllGameTable from "../../pages/MostPointsScoredInAllGameTable";
 import MostPointsScoredInAllGameByTeamTable from "../../pages/MostPointsScoredInAllGameByTeamTable";
 import MostPointsScoredInOneGameTable from "../../pages/MostPointsScoredInOneGameTable";
+import sanitizeData from "../../../utils/sanitizeData";
 
 function Main() {
 
+    const [data, setData] = useState([])
+
+
+    function handleFileUpload(e) {
+        e.preventDefault();
+        const file = e.target.files[0];
+        const reader = new FileReader();
+        if (file != null && file.size > 0) {
+            reader.readAsText(file);
+
+            reader.onload = function () {
+                const dataString = reader.result;
+                const sanitizedData = sanitizeData(dataString);
+
+                setData(sanitizedData);
+            }
+        }
+    }
 
     return (
             <main className="app-content">
+
+                <label className='file-upload'>
+                    Upload your data file:
+                    <input type="file" onChange={handleFileUpload}/>
+                </label>
+
                <Navigation/>
+
                 <Routes>
                     <Route path="/" element={<GeneralStatistic/>}/>
                     <Route path="/most-points-in-game" element={<MostPointsScoredInOneGameTable/>}/>
@@ -25,7 +51,6 @@ function Main() {
                     <Route path="/most-players-points-by-team" element={<MostPointsScoredInAllGameByTeamTable/>}/>
                 </Routes>
             </main>
-
     );
 }
 
---
+++ src/utils/sanitizeData.js
@@ -0,0 +1,19 @@
+function convertStrToArr(str) {
+    return str.split(/(\r\n|\r|\n)/g)
+        .filter(x => x.trim().length !== 0);
+}
+
+function convertArrToMatrix(arr) {
+    return arr.map(x => x
+        .split(',').map(cell => {
+            return cell.trim();
+        }));
+}
+
+function sanitizeData(str) {
+    const arrData = convertStrToArr(str);
+    return convertArrToMatrix(arrData);
+     
+}
+
+export default sanitizeData;

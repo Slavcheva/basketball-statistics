d10c022a0467cdeca008da68beeb402f6b2fe856
Slavcheva <stoyka.slavcheva@gmail.com>
2023-12-13 12:20:13 +0200

    Correct home page text

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -6,7 +6,7 @@ export default function Home({errors}) {
     return (
         <div className="home-page">
             {!errors.length
-                ? <h4>Upload file to see statistics tables</h4>
+                ? <h4>Upload file or load data from database to see statistics</h4>
                 : <>
                     {errors.map(error =>
                         <h4 key={error} className="error">Data on row {error} is invalid</h4>)}
